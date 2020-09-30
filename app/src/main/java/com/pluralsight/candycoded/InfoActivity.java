e63384c912597f0dc6e8a5da5eb41b8aae18b8e2
Farkadi06 <58217000+Farkadi06@users.noreply.github.com>
2020-09-30 11:51:01 +0200

    Start the Google Maps Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,9 +1,12 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -26,7 +29,25 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+public void createMapIntent(View view){
+    // Create a Uri from an intent string. Use the result to create an Intent.
+    // Example latitude = 46.414382, longitude = 10.013988
+    Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
 
+    // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+    Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+
+    // Make the Intent explicit by setting the Google Maps package
+    mapIntent.setPackage("com.google.android.apps.maps");
+
+    if (mapIntent.resolveActivity(getPackageManager())!=null){
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+    else
+        Toast.makeText(this,"do not working",Toast.LENGTH_LONG).show();
+
+}
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
